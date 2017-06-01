ea76586a30984a1da3b23cf40533826ef29ff019
Rohan Judd <rohanjudd@gmail.com>
2017-06-01 21:23:04 +0000

    Some PEP8 fixes

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ from serialmicro import SerialMicro
 micro = SerialMicro()
 hat = Hat()
 
+
 def close():
     print("Shutting Down")
     hat.shutdown()
     micro.disconnect()
     quit()
 
-def goToSleep():
+
+def start_sleep():
     print("Going to Sleep")
     hat.sleep()
 
@@ -28,15 +30,18 @@ menu_rx = Menu("IR Receiver", menu_main)
 menu_tx = Menu("IR Transmitter", menu_main)
 current_menu = menu_main
 
+
 def terminal_test():
     hat.newTerminal()
     for x in range(10):
         hat.write("abcdefghijklmnopqrstuvwxyz")
         time.sleep(0.2)
-    
-def toDo():
+
+
+def to_do():
     print("To Do")
     hat.displayNotification("To Do")
+
 
 def micro_test():
     micro.flush()
@@ -49,20 +54,20 @@ def micro_test():
     time.sleep(0.5)
     micro.send('b')
 
-def rxdisplay_test():
+
+def rx_display_test():
     codes_done = 0
     for x in range(0,32):
         hat.oled.displayRx(x, True, x)
         time.sleep(0.5)
     time.sleep(1)
 
+
 def hat_test():
     hat.testHat()
-    
-def goToMAIN():
-    changeMenu(menu_main)
-    
-def showMenu():
+
+
+def show_menu():
     os.system("clear")
     hat.displayMenu(current_menu)
     print(current_menu.getTitle())
@@ -74,7 +79,7 @@ def showMenu():
 def gotoMenu(menu):
     global current_menu
     current_menu = menu
-    showMenu()
+    show_menu()
     
 def select():
     print("SELECT")
@@ -83,21 +88,21 @@ def incorrectInput():
     print("Incorrect Key")
 
 def populateMenus():
-    menu_main.addEntry("rxDisplay Test", rxdisplay_test)
+    menu_main.addEntry("rxDisplay Test", rx_display_test)
     menu_main.addEntry("IR Transmitter", menu_tx)
     menu_main.addEntry("IR Reciever", menu_rx)
     menu_main.addEntry("Micro Test", micro_test)
     menu_main.addEntry("Terminal Test", terminal_test)
     menu_main.addEntry("Hat Test", hat_test)
 
-    menu_tx.addEntry("Test C10 Tx", toDo)
-    menu_tx.addEntry("Test C16 Tx", toDo)
-    menu_rx.addEntry("Test C10 Rx", toDo)
-    menu_rx.addEntry("Test C16 Rx", toDo)
-    menu_rx.addEntry("Measure Power", toDo)
-    menu_rx.addEntry("Measure Power", toDo)
-    menu_rx.addEntry("Measure Power", toDo)
-    menu_rx.addEntry("Measure Power", toDo)
+    menu_tx.addEntry("Test C10 Tx", to_do)
+    menu_tx.addEntry("Test C16 Tx", to_do)
+    menu_rx.addEntry("Test C10 Rx", to_do)
+    menu_rx.addEntry("Test C16 Rx", to_do)
+    menu_rx.addEntry("Measure Power", to_do)
+    menu_rx.addEntry("Measure Power", to_do)
+    menu_rx.addEntry("Measure Power", to_do)
+    menu_rx.addEntry("Measure Power", to_do)
     menu_shutdown.addEntry("Back to Main", menu_main)
     menu_shutdown.addEntry("Shutdown", close)
 
@@ -110,7 +115,7 @@ def processButtonInput():
         sleepTimer += 1
         time.sleep(0.05)
     if sleepTimer >= 500:
-        goToSleep()
+        start_sleep()
         while state == hat.NONE and shutdownTimer < 500:
             state = hat.getButtonState()
             shutdownTimer += 1
@@ -183,13 +188,13 @@ def main():
     hat.splash()
     time.sleep(4)
 
-    rxdisplay_test()
+    rx_display_test()
     
     gotoMenu(menu_main)
     while(True):
         #processInput()
         processButtonInput()
-        showMenu()
+        show_menu()
 
 if __name__ == '__main__':
 	main()
