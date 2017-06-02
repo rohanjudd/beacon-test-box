4461f48a9a5326102527333ecabf59a48a1d0021
Rohan <rohanjudd@gmail.com>
2017-06-02 22:55:46 +0100

    Simplification and moving constants to seperate module

--- hat.py
+++ hat.py
@@ -1,111 +1,70 @@
 import Adafruit_GPIO as GPIO
 import time
+import hat_constants
 from oled import Oled
 
 
 class Hat:
-    STATUS_PIN = 5
-    FAIL_PIN = 6
-    PASS_PIN = 13
-    UP_PIN = 17
-    DOWN_PIN = 18
-    BACK_PIN = 27
-    SELECT_PIN = 22
-
-    NONE = 0
-    UP = 1
-    DOWN = 2
-    BACK = 3
-    SELECT = 4
-
-    DEFAULT_FLASH = 0.2
-
     def __init__(self):
         self.oled = Oled()
         self.gpio = GPIO.get_platform_gpio()
 
-        self.gpio.setup(self.STATUS_PIN, GPIO.OUT)
-        self.gpio.setup(self.FAIL_PIN, GPIO.OUT)
-        self.gpio.setup(self.PASS_PIN, GPIO.OUT)
+        self.gpio.setup(hat_constants.STATUS_PIN, GPIO.OUT)
+        self.gpio.setup(hat_constants.RED_PIN, GPIO.OUT)
+        self.gpio.setup(hat_constants.GREEN_PIN, GPIO.OUT)
 
-        self.gpio.setup(self.UP_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        self.gpio.setup(self.DOWN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        self.gpio.setup(self.BACK_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        self.gpio.setup(self.SELECT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.gpio.setup(hat_constants.UP_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.gpio.setup(hat_constants.DOWN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.gpio.setup(hat_constants.BACK_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.gpio.setup(hat_constants.SELECT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-        self.lastButtonState = self.NONE
+        self.lastButtonState = hat_constants.NONE
 
-    def set_status_led(self, state):
-        self.gpio.output(self.STATUS_PIN, state)
-
-    def set_fail_led(self, state):
-        self.gpio.output(self.FAIL_PIN, state)
-
-    def set_pass_led(self, state):
-        self.gpio.output(self.PASS_PIN, state)
+    def set_led_states(self, status=False, red=False, green=False):
+        self.gpio.output(hat_constants.STATUS_PIN, status)
+        self.gpio.output(hat_constants.RED_PIN, red)
+        self.gpio.output(hat_constants.GREEN_PIN, green)
 
     def get_up_state(self):
-        return not self.gpio.input(self.UP_PIN)
+        return not self.gpio.input(hat_constants.UP_PIN)
 
     def get_down_state(self):
-        return not self.gpio.input(self.DOWN_PIN)
+        return not self.gpio.input(hat_constants.DOWN_PIN)
 
     def get_back_state(self):
-        return not self.gpio.input(self.BACK_PIN)
+        return not self.gpio.input(hat_constants.BACK_PIN)
 
     def get_select_state(self):
-        return not self.gpio.input(self.SELECT_PIN)
+        return not self.gpio.input(hat_constants.SELECT_PIN)
 
     def get_button_state(self):
-        state = self.NONE
+        state = hat_constants.NONE
         if self.get_up_state():
-            state = self.UP
+            state = hat_constants.UP
         elif self.get_down_state():
-            state = self.DOWN
+            state = hat_constants.DOWN
         elif self.get_back_state():
-            state = self.BACK
+            state = hat_constants.BACK
         elif self.get_select_state():
-            state = self.SELECT
+            state = hat_constants.SELECT
         if state == self.lastButtonState:
             self.lastButtonState = state
-            return self.NONE
+            return hat_constants.NONE
         self.lastButtonState = state
         return state
 
     def hat_test(self):
-        self.set_leds(True)
+        self.set_led_states(status=True, red=True, green=True)
         time.sleep(0.5)
-        self.set_leds(False)
-
-    def set_leds(self, state):
-        self.set_status_led(state)
-        self.set_fail_led(state)
-        self.set_pass_led(state)
-
-    def flash_status(self):
-        self.set_status_led(True)
-        time.sleep(0.2)
-        self.set_status_led(False)
-
-    def flash_fail(self):
-        self.set_fail_led(True)
-        time.sleep(0.2)
-        self.set_fail_led(False)
-
-    def flash_pass(self):
-        self.set_pass_led(True)
-        time.sleep(0.2)
-        self.set_pass_led(False)
+        self.set_led_states(status=False, red=False, green=False)
 
     def shutdown(self):
-        self.oled.start_terminal()
         self.oled.display_notification("Shutting Down")
         time.sleep(1)
         self.oled.blank()
-        self.set_leds(False)
+        self.set_led_states(status=False, red=False, green=False)
 
     def sleep(self):
-        self.oled.start_terminal()
         self.oled.display_notification("Going to Sleep")
         time.sleep(1)
         self.oled.blank()
